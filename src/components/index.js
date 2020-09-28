88a4963f6ffdd4e2ee80dc8c0ba4f2d8db6aa2ad
EdinK1 <edinkaymakchi@gmail.com>
2020-09-28 00:44:40 -0700

    remove unused components

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ import Nav from './Nav'
 import Spinner from './Spinner'
 import Editor from './Editor'
 import CodeBox from './CodeBox'
-import Toast from './Toast'
-export { Nav, Spinner, Editor, CodeBox, Toast }
+export { Nav, Spinner, Editor, CodeBox }
